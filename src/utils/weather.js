a5b90f441f48917de8a0fc5f39c8cb6ecdafdcd8
garymor <garymor.il@gmail.com>
2021-06-28 20:46:00 +0300

    small_fix_2

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch');
-const url = 'http://api.weatherstack.com/current?access_key=4edb69d0edd445720fa6949592691ad2&query=%20%22tel%20aviv%22' ;
 
 let weather = {
     forecast:''
